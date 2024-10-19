91c251d4df28b5f47caa1f677924208a56d6ce0b
Erik Polzin <eriktpol@gmail.com>
2024-10-19 19:45:45 +0200

    Data model strings

--- transwage/accounts/models.py
+++ transwage/accounts/models.py
@@ -9,6 +9,9 @@ class Business(models.Model):
     name = models.CharField(max_length=64)
     employers = models.ManyToManyField(User)
 
+    def __str__(self):
+        return f"Business: {self.name}"
+
     def employees(self) -> Iterable[User]:
         # Double for loop, must be a more efficient way to do this
         return [c.employee for j in self.jobs.prefetch_related("contracts") for c in j.contracts]
@@ -17,6 +20,9 @@ class Business(models.Model):
 class Job(models.Model):
     """A description of a particular job type."""
 
+    def __str__(self):
+        return f"Job: {self.name}"
+
     business = models.ForeignKey(Business, on_delete=models.CASCADE)
     hourly_wage = models.FloatField()
     name = models.CharField(max_length=64)
@@ -24,6 +30,9 @@ class Job(models.Model):
 
 class Contract(models.Model):
     """A contract for an employee doing a job in a particular time period."""
+
+    def __str__(self):
+        return f"Contract: {self.employee.username} - {self.job.name}"
 
     job = models.ForeignKey(Job, on_delete=models.CASCADE)
     employee = models.ForeignKey(User, on_delete=models.CASCADE)
